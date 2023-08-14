7f880366353ac73863b28d058cfe8bfeedbe1bb3
Zach <zachta2020@gmail.com>
2023-08-14 15:44:00 -0500

    Fixed bug preventing sprite visibility.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,38 @@
-//! Renders a 2D scene containing a single, moving sprite.
-
 use bevy::prelude::*;
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
-        .add_systems(Update, sprite_movement)
-        .run();
+//Resources
+#[derive(Resource)]
+struct UpdateTimer(Timer);
+
+#[derive(Resource)]
+struct TimerCounter {i : u32}
+
+impl Default for TimerCounter {
+    fn default() -> Self {
+        TimerCounter{i : 0}
+    }
 }
 
+//Components
 #[derive(Component)]
 enum Direction {
     Up,
     Down,
 }
 
+//Systems
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("assets/icon.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
+            texture: asset_server.load("icon.png"),
+            transform: Transform::from_scale(Vec3::new(0.25, 0.25, 1.)),
             ..default()
         },
         Direction::Up,
     ));
 }
 
-/// The sprite is animated by changing its translation depending on the time that has passed since
-/// the last frame.
 fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
     for (mut logo, mut transform) in &mut sprite_position {
         match *logo {
@@ -43,4 +46,33 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
             *logo = Direction::Up;
         }
     }
+}
+
+fn timer_print(time: Res<Time>, mut timer: ResMut<UpdateTimer>, mut counter: ResMut<TimerCounter>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        println!("Counter: {}", counter.i);
+        counter.i += 1;
+        if counter.i == 10 {
+            counter.i = 0;
+        }
+    }
+}
+
+//Plugins
+pub struct CounterPlugin;
+impl Plugin for CounterPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(UpdateTimer(Timer::from_seconds(1.0, TimerMode::Repeating)))
+        .insert_resource(TimerCounter{i:0})
+        .add_systems(Update, timer_print);
+    }
+}
+
+//Main
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, CounterPlugin))
+        .add_systems(Startup, setup)
+        .add_systems(Update, sprite_movement)
+        .run();
 }
